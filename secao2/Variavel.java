ef6829af7bc1a863e418d51e155c02ff1383db54
lucianobrsts <lucianobrsts@yahoo.com.br>
2025-01-07 11:10:02 -0300

    O que são variáveis

---
+++ secao2/Variavel.java
@@ -0,0 +1,15 @@
+package secao2;
+
+public class Variavel {
+
+    public static void main(String[] args) {
+
+        // 1- O que são variáveis
+        // tipo -> nome -> atribuir um valor;
+        String nome = "Luciano";
+
+        // nome => "Matheus"
+        System.out.println(nome);
+
+    }
+}
