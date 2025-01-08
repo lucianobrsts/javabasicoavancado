5319d3ee35c77aaa519d5108e623ad56988b14cd
lucianobrsts <lucianobrsts@yahoo.com.br>
2025-01-08 17:06:01 -0300

    A classe scanner

---
+++ secao2/TesteScanner.java
@@ -0,0 +1,21 @@
+package secao2;
+
+import java.util.Scanner;
+
+public class TesteScanner {
+
+    public static void main(String[] args) {
+
+        // 1- Testando scanner
+        Scanner entrada = new Scanner(System.in);
+        System.out.print("Digite o seu nome: ");
+        String nome = entrada.nextLine();
+        System.out.println("Olá " + nome + "!");
+
+        System.out.print("Digite um valor numérico: ");
+        int valor = entrada.nextInt();
+        System.out.println("O número digitado foi: " + valor);
+
+        entrada.close();
+    }
+}
