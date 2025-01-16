b3ace92269935e355565d7cdf9673198b020cf02
lucianobrsts <lucianobrsts@yahoo.com.br>
2025-01-15 22:44:46 -0300

    Tabuada finalizada

--- secao3/Tabuada.java
+++ secao3/Tabuada.java
@@ -7,29 +7,36 @@ import java.util.Scanner;
  * receber um número pelo scanner, exibir a tabuada do mesmo
  */
 public class Tabuada {
-    
+
     public static void main(String[] args) {
-        
+
         Scanner entrada = new Scanner(System.in);
+        String textoInicial = "Digite um número entre 1 e 10 para que sua tabuada seja exibida: ";
         int limiteTabuada = 10;
-        System.out.print("Digite um número entre 1 e 10 para que sua tabuada seja exibida: ");
-        int valorParaTabuada = entrada.nextInt();
+        int valorParaTabuada = 0;
+        char repetirExibirTabuada = 's';
 
-        if(valorParaTabuada > 10 || valorParaTabuada < 1) {
-            System.out.println("Digite um valor entre 1 e 9!");
+        while (valorParaTabuada > 10 || valorParaTabuada < 1) {
+            System.out.print(textoInicial);
+            valorParaTabuada = entrada.nextInt();
         }
 
-        for (int i = 1; i <= limiteTabuada; i++) {
-            System.out.print(i + " x " + valorParaTabuada + " = " + (i * valorParaTabuada));
-            System.out.println();
+        while (repetirExibirTabuada != 'n') {
+            for (int i = 1; i <= limiteTabuada; i++) {
+                System.out.print(i + " x " + valorParaTabuada + " = " + (i * valorParaTabuada));
+                System.out.println();
+            }
+
+            System.out.println("Deseja exibir outra tabuada? ");
+            System.out.println("Digite s ou n");
+            repetirExibirTabuada = entrada.next().charAt(0);
+
+            if(repetirExibirTabuada == 's') {
+                System.out.print(textoInicial);
+                valorParaTabuada = entrada.nextInt();
+            }
         }
 
-        for (int i = 1; i <= limiteTabuada; i++) {
-            System.out.print(i + " x " + valorParaTabuada + " = " + (i * valorParaTabuada));
-            System.out.println();
-        }
-
-
-
+        entrada.close();
     }
 }
