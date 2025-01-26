b281aaeeb110b11f1e2fb440bd834253a904d164
lucianobrsts <lucianobrsts@yahoo.com.br>
2025-01-26 10:31:38 -0300

    Operador Lógico OR

--- secao5/OperadoresLogicos.java
+++ secao5/OperadoresLogicos.java
@@ -8,7 +8,7 @@ public class OperadoresLogicos {
         boolean temCarteiraDeMotorista = true;
 
         System.out.println(idade >= 18 && temCarteiraDeMotorista);
-        System.out.println(idade >= 18 && temCarteiraDeMotorista  == true);
+        System.out.println(idade >= 18 && temCarteiraDeMotorista == true);
 
         boolean carteiraVencida = false;
 
@@ -17,13 +17,32 @@ public class OperadoresLogicos {
         int a = 10;
         int b = 20;
 
-        if(a > 5 && b > 10) {
+        if (a > 5 && b > 10) {
             System.out.println("Deu certo!");
         }
 
-        if(a > 55 && b > 10) {
+        if (a > 55 && b > 10) {
             System.out.println("Deu certo 2!");
         }
+
+        boolean estaChovendo = false;
+        boolean temGuardaChuva = true;
+
+        System.out.println(estaChovendo || temGuardaChuva);
+        System.out.println(false || false);
+        System.out.println(10 > 20 && 100 == 200);
+
+        // O usuário pode participar do clube
+        // ele precisa ter > 16 anos ou ser membro
+        int idade2 = 16;
+        boolean ehMembro = true;
+
+        if(idade2 > 16 || ehMembro) {
+            System.out.println("Você pode entrar!");
+        } else {
+            System.out.println("Entrada negada!");
+        }
+
 
     }
 
