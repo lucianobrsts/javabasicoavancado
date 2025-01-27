8dc72b4e8c5480d7043f3ee478bb4b1de9a49d67
lucianobrsts <lucianobrsts@yahoo.com.br>
2025-01-26 21:50:42 -0300

    Condicional Ternária

---
+++ secao5/Condicionais2.java
@@ -0,0 +1,13 @@
+package secao5;
+
+public class Condicionais2 {
+    
+    public static void main(String[] args) {
+        
+        int numero = 11;
+
+        String resultado = (numero % 2 == 0) ? "Par" : "Ímpar";
+        System.out.println(resultado);
+
+    }
+}
