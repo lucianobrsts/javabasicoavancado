f256eed584a5d19e72147990ab0dca5a58a032be
lucianobrsts <lucianobrsts@yahoo.com.br>
2025-01-26 21:58:30 -0300

    If else aninhado

--- secao5/Condicionais2.java
+++ secao5/Condicionais2.java
@@ -1,13 +1,25 @@
 package secao5;
 
 public class Condicionais2 {
-    
+
     public static void main(String[] args) {
-        
+
         int numero = 11;
 
         String resultado = (numero % 2 == 0) ? "Par" : "Ímpar";
         System.out.println(resultado);
 
+        int idade = 18;
+        boolean temCarteira = true;
+
+        if (idade >= 18) {
+            if (temCarteira) {
+                System.out.println("Pode dirigir");
+            } else {
+                System.out.println("Precisa ter a habilitação para dirigir!");
+            }
+        } else {
+            System.out.println("Você não pode dirigir ainda!");
+        }
     }
 }
