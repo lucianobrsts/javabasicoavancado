4611f6407cf1e0207b084d4b1b7122517e497d6c
lucianobrsts <lucianobrsts@yahoo.com.br>
2025-01-28 23:42:39 -0300

    Criando a Primeira função

---
+++ secao7/ClassificacaoProduto.java
@@ -0,0 +1,13 @@
+package secao7;
+
+import java.util.Scanner;
+
+public class ClassificacaoProduto {
+    
+    public static void main(String[] args) {
+        
+        Scanner teclado = new Scanner(System.in);
+        
+
+    }
+}
---
+++ secao8/Funcoes.java
@@ -0,0 +1,14 @@
+package secao8;
+
+public class Funcoes {
+    
+    public static void main(String[] args) {
+        saudacao();
+
+    }
+
+    public static void saudacao() {
+        System.out.println("Olá esta é a minha primeira função!");
+    }
+
+}
